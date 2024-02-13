68df2f8a88ffcaa33d343cf6d12194112c88a372
Matheus Carvie <matheuspowerdev@gmail.com>
2024-02-13 18:00:25 -0300

    Ajuste no multer

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -4,7 +4,7 @@ function Upload() {
     const folderPath: string = "tmp";
     return multer.diskStorage({
         destination: ((req, file, callback) => {
-            return callback(null, `${__dirname}/../${folderPath}`);
+            return callback(null, `${__dirname}/../../${folderPath}`);
         }),
         filename: ((req, file, callback) => {
             return callback(null, `${Date.now()}-${file.originalname}`);
